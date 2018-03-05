5e7c97e945182b63841b336a1a0cb9cac58154f0
Cody Brunner <rockchalkwushock@icloud.com>
2018-03-05 11:43:31 -0600

    fix(root): fixes conditional for .env support at dev time vs prod time

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // When running on CI don't use .env
-if (!process.env.CI) {
+if (process.env.CI === undefined) {
   require('dotenv-safe').load()
 }
+
 const microService = require('./server')
 
 module.exports = microService
