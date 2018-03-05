0b6c9693e1bb98875f73c00653124fb2db876e76
Cody Brunner <rockchalkwushock@icloud.com>
2018-03-05 11:49:29 -0600

    fix(root): removes dotenv-safe

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-// When running on CI don't use .env
-if (process.env.CI === undefined) {
-  require('dotenv-safe').load()
-}
-
 const microService = require('./server')
 
 module.exports = microService
